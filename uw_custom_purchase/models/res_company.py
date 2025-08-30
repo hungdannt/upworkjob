7cd3d2295464b5be21558f04c2222265ac9ce299
Dan NT <caucahotran@gmail.com>
2025-08-30 12:59:45 +0700

    fix

--- uw_custom_purchase/models/res_company.py
+++ uw_custom_purchase/models/res_company.py
@@ -4,6 +4,4 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    address = fields.Char(string='Address Line 1')
     cin_number = fields.Char(string='CIN Number')
-    gstin = fields.Char(string='GSTIN', help="GST Identification Number")
--- uw_custom_purchase/models/res_partner.py
+++ uw_custom_purchase/models/res_partner.py
@@ -4,5 +4,4 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    gstin = fields.Char(string='GSTIN')
     code = fields.Char(string='Ref Code')
