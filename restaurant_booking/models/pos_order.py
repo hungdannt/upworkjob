548a77f61e4519a895f31b86535468ac78f96bdb
Dan NT <caucahotran@gmail.com>
2025-10-08 16:14:25 +0700

    add state booking to done when order paid

--- restaurant_booking/models/pos_order.py
+++ restaurant_booking/models/pos_order.py
@@ -4,3 +4,10 @@ class PosOrder(models.Model):
     _inherit = "pos.order"
 
     booking_id = fields.Many2one("restaurant.booking", string="Booking Ref")
+
+    def write(self, vals):
+        if 'state' in vals:
+            if vals['state'] == 'paid':
+                self.booking_id.state = 'done'
+        return super(PosOrder, self).write(vals)
+            
